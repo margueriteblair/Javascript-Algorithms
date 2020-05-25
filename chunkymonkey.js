acf9042858cd0b20d0c210cadf2badde56a1f950
margueriteblair <62972798+margueriteblair@users.noreply.github.com>
2020-05-25 17:36:58 -0400

    Add files via upload
    second function made to chunk an array

--- chunkymonkey.js
+++ chunkymonkey.js
@@ -11,14 +11,23 @@ function chunkArrayInGroups(arr, size) {
     console.log(chunkedArray)
 }
   
-  chunkArrayInGroups(["a", "b", "c", "d"], 2);
-  chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3)
-  chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2)
-  chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4)
-  chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3)
-  chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2)
+//   chunkArrayInGroups(["a", "b", "c", "d"], 2);
+//   chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3)
+//   chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2)
+//   chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4)
+//   chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3)
+//   chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2)
 
-// let array = 
-// array.splice(0, 3)
-// console.log(array)
+
+function chunkArrayInGroups2(arr, size) {
+    const chunkedArray = [];
+    let index = 0;
+    while (index < arr.length) {
+        chunkedArray.push(arr.slice(index, size+index))
+        index+=size;
+    }
+    console.log(chunkedArray)
+}
+chunkArrayInGroups2(["a", "b", "c", "d"], 2);
+chunkArrayInGroups2([0, 1, 2, 3, 4, 5, 6, 7, 8], 2)
   
