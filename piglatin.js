674c4fa8941d72399a2b99e187d6b6bc6371a359
Marguerite Blair <marguerite.blair@gmail.com>
2020-06-29 14:21:52 -0400

    two functions that work as pig latin translators

---
+++ piglatin.js
@@ -0,0 +1,31 @@
+function translatePigLatin(str) {
+    let consonantRegex = /^[^aeiou]+/;
+    let myConsonants = str.match(consonantRegex);
+    return myConsonants !== null
+      ? str
+          .replace(consonantRegex, "")
+          .concat(myConsonants)
+          .concat("ay")
+      : str.concat("way");
+  }
+  
+  translatePigLatin("consonant");
+
+  function translatePigLatin2() {
+  var pigLatin = "";
+  var regex = /[aeiou]/gi;
+
+  // Check if the first character is a vowel
+  if (str[0].match(regex)) {
+    pigLatin = str + "way";
+  } else if (str.match(regex) === null) {
+    pigLatin = str + "ay";
+  } else {
+    var vowelIndice = str.indexOf(str.match(regex)[0]);
+    pigLatin = str.substr(vowelIndice) + str.substr(0, vowelIndice) + "ay";
+  }
+
+  return pigLatin;
+}
+
+translatePigLatin2("consonant");
