be0b7f180c4b96a7449c79094ae960efc729c2b5
Marguerite Blair <marguerite.blair@gmail.com>
2020-07-10 00:29:39 -0400

    dna pairs pushed to overall array with chunks

---
+++ dnapairing.js
@@ -0,0 +1,24 @@
+function pairElement(str) {
+    let dnaPairs = [];
+    let chunkedDNAPairs = [];
+    let index = 0;
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === 'G') {
+        dnaPairs.push('G', 'C')
+      } else if (str[i] === 'C') {
+        dnaPairs.push('C', 'G')
+      } else if (str[i] === 'A') {
+        dnaPairs.push('A', 'T')
+      } else if (str[i] === 'T') {
+        dnaPairs.push('T', 'A')
+      }
+    }
+    while (index < dnaPairs.length) {
+      chunkedDNAPairs.push(dnaPairs.slice(index, 2+index));
+      index+=2;
+    }
+    return chunkedDNAPairs
+  }
+  
+  pairElement("GCG");
+  pairElement("ATCGA")
