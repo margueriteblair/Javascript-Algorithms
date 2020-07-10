8563d834093d8f6c0709b0f29c8287295ca19c91
Marguerite Blair <marguerite.blair@gmail.com>
2020-07-10 00:48:52 -0400

    drop elements algo but with splice

--- dropelements.js
+++ dropelements.js
@@ -11,3 +11,18 @@ function dropElements(arr, func) {
     
   }
   dropElements([1, 2, 3, 4], function(n) {return n > 5;})
+
+  function dropElements2(arr, func) {
+    for (var i = 0; i < arr.length; i++) {
+      if (func(arr[0])) {
+      } else if (func(arr[0]) !== true){
+        console.log(arr, "before splice")
+        console.log("_______")
+        arr.splice(0, 1);
+        console.log(arr)
+      } 
+    }
+    return arr;
+    
+  }
+  dropElements2([1, 2, 3, 4], function(n) {return n > 5;})
