b18a272120831412ce4471070224619e7627047c
Marguerite Blair <marguerite.blair@gmail.com>
2020-07-11 14:28:02 -0400

    using repeat method

--- repeatastring.js
+++ repeatastring.js
@@ -6,4 +6,11 @@ function repeatAString(string, num) {
     console.log(newString);
 }
 
-repeatAString("We love to code.", 4);
+//repeatAString("We love to code.", 4);
+
+function repeatAString2(string, num) {
+    string = string.repeat(num);
+    console.log(string)
+}
+
+repeatAString2("hello", 4);
