760cbb030e313c505e64a7097e11e64b262899b0
Marguerite Blair <marguerite.blair@gmail.com>
2020-07-13 23:14:24 -0400

    spread operator and math max

--- largestnumber.js
+++ largestnumber.js
@@ -17,3 +17,13 @@ function largestOfFour(arr) {
 }
 
 largestOfFour(([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+
+function largestOfFour2(arr) {
+    let newArray = [];
+    for (let i = 0; i<arr.length;i++) {
+      newArray.push(Math.max(...arr[i]))
+    }
+    return newArray;
+  }
+  
+  largestOfFour2([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
