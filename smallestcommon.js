0f7a0d36f5f75305eeea4d1f4e6e59be611bd54d
Marguerite Blair <marguerite.blair@gmail.com>
2020-07-14 20:20:58 -0400

    smallest common multiple

---
+++ smallestcommon.js
@@ -0,0 +1,30 @@
+function smallestCommons(arr) {
+    arr = arr.sort(function(a, b) {
+      return a-b;
+    })
+    for (let i = arr[0]+1; i < arr[1]; i++) {
+      arr.push(i)
+    }
+    arr = arr.sort(function(a, b) {
+      return a-b;
+    })
+  
+    let lcm = arr[0]
+    for (let i = 0; i < arr.length; i++) {
+      let GCM = gcd(lcm, arr[i])
+      lcm = lcm*arr[i]/GCM
+    }
+    return lcm;
+      function gcd(x, y) {
+      // Implements the Euclidean Algorithm
+      if (y === 0) return x;
+      else return gcd(y, x % y);
+    }
+  }
+  
+  
+  
+  smallestCommons([1,5]);
+  smallestCommons([23, 18])
+  smallestCommons([5, 1])
+  
