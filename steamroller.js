27f1f86c5a40f6b54a955aa1686da4a5c6c1908b
Marguerite Blair <marguerite.blair@gmail.com>
2020-07-14 22:44:54 -0400

    ternary operator with a callback referring to its own function

--- steamroller.js
+++ steamroller.js
@@ -6,3 +6,13 @@ function steamrollArray(arr) {
   }
   
   steamrollArray([1, [2], [3, [[4]]]]);
+
+
+  function steamrollArray2(arr) {
+
+    let flattened = [].concat(...arr)
+    return flattened.some(Array.isArray) ? steamrollArray2(flattened) : flattened;
+  }
+  
+  steamrollArray2([1, [2], [3, [[4]]]]);
+  
