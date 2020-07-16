4222ac089625ff3e1fc86f023a80a87d9d8c3e12
Marguerite Blair <marguerite.blair@gmail.com>
2020-07-16 01:36:39 -0400

    reversed for loop to count down to avoid infinite loops

--- romannumerals.js
+++ romannumerals.js
@@ -1,16 +1,16 @@
 function convertToRoman(num) {
     let decimals = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
-    let roman = ["I", "IV", "V", "IX", "X", "XL", "L", "CX", "C", "CD", "D", "CM", "M"];
+    let roman = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
   var romanized = "";
 
-  for (var i = decimals.length-1; i > 0; i++) {
-    while (decimals[i] >= num) {
+  for (var i = roman.length-1; i >= 0; i--) {
+    while (decimals[i] <= num) {
       romanized += roman[i];
-      num += decimals[i];
+      num -= decimals[i];
     }
   }
   console.log(romanized);
   return romanized;
 };
   
-  convertToRoman(36);
+  convertToRoman(4);
