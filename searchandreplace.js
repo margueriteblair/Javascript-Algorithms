8394a43cb253d914bfc901162bc4a6a39f173b39
Marguerite Blair <marguerite.blair@gmail.com>
2020-07-21 17:51:52 -0400

    will's version of search and replace

--- searchandreplace.js
+++ searchandreplace.js
@@ -27,3 +27,20 @@ function myReplace(str, before, after) {
   }
   
   myReplace2("He is Sleeping on the couch", "Sleeping", "sitting")
+
+  function myReplace3(str, before, after) {
+    let regex = /^[A-Z]/
+    let splitWord = after.split('')
+    let match = regex.exec(before)
+    let word = ''
+    if(Boolean(match) == true) {
+      let letter = splitWord.shift().toUpperCase()
+      splitWord.unshift(letter);
+      splitWord = splitWord.join('')
+      word = str.replace(before, splitWord)
+    } else {
+   word = str.replace(before, after)
+  }
+  return word;
+  }
+  console.log(myReplace3("A quick brown fox Jumped over the lazy dog", "Jumped", "leaped"));
