430541317dc634c1c2acb2efc00ed13e486d8919
Marguerite Blair <marguerite.blair@gmail.com>
2020-07-23 19:39:52 -0400

    charcode at method

--- missingletter.js
+++ missingletter.js
@@ -19,3 +19,15 @@ function fearNotLetter(str) {
   
   //fearNotLetter("abcdefghjklmno")
   fearNotLetter("abcdefghijklmnopqrstuvwxyz")
+
+  function fearNotLetter(str) {
+    for (let i = 0; i < str.length; i++) {
+      if (str.charCodeAt(i) - str.charCodeAt(i - 1) > 1) {
+        console.log(String.fromCharCode(str.charCodeAt(i - 1) + 1));
+        //return String.fromCharCode(str.charCodeAt(i - 1) + 1);
+      } 
+    }
+  }
+  
+  fearNotLetter("abce");
+  fearNotLetter("abcdefghjklmno")
