853f74bf9d7d4cefe19d0554c9b315268381cca5
Marguerite Blair <marguerite.blair@gmail.com>
2020-08-09 21:59:27 -0400

    sieve of erasthenes

--- primetest.js
+++ primetest.js
@@ -16,3 +16,38 @@ function sumPrimes(num) {
 }
 
 sumPrimes(10);
+
+
+
+
+function sumPrimes2(num) {
+  var res = 0;
+
+  function getPrimes2(max) {
+    var sieve = [];
+    var i;
+    var j;
+    var primes = [];
+    for (i = 2; i <= max; i++) {
+      if (!sieve[i]) {
+        primes.push(i);
+        for (j = i << 1; j <= max; j += i) {
+          sieve[j] = true;
+        }
+      }
+    }
+
+    return primes;
+  }
+
+  // Add the primes
+  var primes = getPrimes2(num);
+  for (var p = 0; p < primes.length; p++) {
+    res += primes[p];
+  }
+
+  return res;
+}
+
+// test here
+sumPrimes2(10);
