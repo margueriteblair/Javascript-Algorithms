09d97dfaae25ad25f56db60ce48c61d9d62ea023
Marguerite Blair <marguerite.blair@gmail.com>
2020-08-27 19:51:50 -0400

    created homework stack array and push and pop to manipulate

---
+++ stackstructures.js
@@ -0,0 +1,8 @@
+//stack data structures use arrays and push and pop for a last in first out type of deal
+
+var homeworkStack = ["BIO12","HIS80","MAT122","PSY44"];
+// Only change code below this line
+homeworkStack.pop()
+homeworkStack.push("CS50")
+
+console.log(homeworkStack)
