85709bae83b6efc54be5168b5e277b30f39693d4
Marguerite Blair <marguerite.blair@gmail.com>
2020-08-28 09:21:25 -0400

    pairwise function to sum indices

--- marylamb.js
+++ marylamb.js
@@ -1,22 +1,42 @@
-let string = `Mary had a little lamb it’s fleece was white as snow, everywhere that Mary went her lamb was sure to go. Baby girl loved those lamb chopzzz!`;
-let lowerCase = string.toLowerCase();
-let lettersArray = [];
-const chunkedArray = [];
-
-for (let i = 0; i < lowerCase.length; i++) {
-    if (lowerCase[i].match(/\w/)) {
-        lettersArray.push(lowerCase[i])
+function maryHadALittleLamb() {
+    let string = `Mary had a little lamb it’s fleece was white as snow, everywhere that Mary went her lamb was sure to go. Baby girl loved those lamb chopzzz!`;
+    let lowerCase = string.toLowerCase();
+    let lettersArray = [];
+    const chunkedArray = [];
+    
+    for (let i = 0; i < lowerCase.length; i++) {
+        if (lowerCase[i].match(/\w/)) {
+            lettersArray.push(lowerCase[i])
+        }
     }
-}
-
-lettersArray.sort();
-lettersArray = lettersArray.join("");
-
-for (let i = 0; i < lettersArray.length; i++) {
-    if (lettersArray[i] !== lettersArray[i-1]) {
-        chunkedArray.push(lettersArray.substring(lettersArray.indexOf(lettersArray[i-1]), i))
+    
+    lettersArray.sort();
+    lettersArray = lettersArray.join("");
+    
+    for (let i = 0; i < lettersArray.length; i++) {
+        if (lettersArray[i] !== lettersArray[i-1]) {
+            chunkedArray.push(lettersArray.substring(lettersArray.indexOf(lettersArray[i-1]), i))
+        }
     }
+    for (let i = 0; i < chunkedArray.length; i++) {
+        console.log(`${chunkedArray[i][0]} count: ${chunkedArray[i].length} `);
+    }
+    
 }
-for (let i = 0; i < chunkedArray.length; i++) {
-    console.log(`${chunkedArray[i][0]} count: ${chunkedArray[i].length} `);
-}
+//console.log(maryHadALittleLamb())
+
+// function maryHadALittleLamb2() {
+//     let string = `Mary had a little lamb it’s fleece was white as snow, everywhere that Mary went her lamb was sure to go. Baby girl loved those lamb chopzzz!`;
+//     string = string.toLowerCase().split("");
+//     string = string.sort();
+//     let chunkedArray = [];
+
+//     for (let i = 0; i < string.length; i++) {
+//         if (string[i] !== string[i-1]) {
+//             chunkedArray.push(string.splice(string.indexOf(string[i-1]), i))
+//         }
+//     }
+//     console.log(chunkedArray)
+// }
+
+// maryHadALittleLamb2();
---
+++ pairwise.js
@@ -0,0 +1,16 @@
+function pairwise(arr, arg) {
+    let sum = 0;
+    let checkIndex = [];
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = i+1; j< arr.length; j++) {
+        if (arr[i]+arr[j] === arg && checkIndex.indexOf(i) == -1 && checkIndex.indexOf(j) == -1) {
+          checkIndex.push(i)
+          checkIndex.push(j);
+          sum += i+j
+        }
+      }
+    }
+    return sum;
+  }
+  
+  pairwise([1,4,2,3,0,5], 7);
