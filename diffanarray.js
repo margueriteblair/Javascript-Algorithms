9507f4dff0dffea9c6b1f64fc1002420a01f1035
Marguerite Blair <marguerite.blair@gmail.com>
2020-08-29 07:47:01 -0400

    diff an array with the not operator

--- diffanarray.js
+++ diffanarray.js
@@ -13,5 +13,25 @@ function diffArray(arr1, arr2) {
     console.log(newArr)
     return newArr;
   }
+
+  function diffArray2(arr1, arr2) {
+    //return the symmetric difference of the new array
+    var newArr = [];
+    for (let i = 0; i < arr1.length; i++) {
+      if (!arr2.includes(arr1[i])) {
+        newArr.push(arr1[i])
+      } 
+    }
+    for (let i = 0; i < arr2.length; i++) {
+    if (!arr1.includes(arr2[i])) {
+      newArr.push(arr2[i])
+    } 
+  }
+  
+    return newArr;
+  }
+  
+  diffArray2([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+  
   
   diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
