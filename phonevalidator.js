f2141d722150bf58a20f75ec4c36df71b781c106
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-09 11:18:02 -0400

    str match regex with multifaceted if statement to return valid telephone numbers

--- phonevalidator.js
+++ phonevalidator.js
@@ -13,4 +13,24 @@ function telephoneCheck(str) {
     return regex.test(str);
   }
   telephoneCheck2("555-555-5555");
+
+  
+
+  function telephoneCheck3(str) {
+    if (str.match(/\d/g).length === 10) {
+      return true;
+    } else if (str.match(/\d/g).length === 11 && str.match(/\d/g)[0] != 1) {
+      console.log(false)
+      return false;
+    } else if (str.match(/\d/g).length === 11 && str.match(/\d/g)[0] == 1) {
+      console.log(true)
+      return true;
+    } else if (str.match(/\d/g).length < 10 || str.match(/\d/g).length > 11) {
+      return false
+    }
+  }
+  
+  telephoneCheck3("1 555)555-5555")
+  telephoneCheck3("10 (757) 622-7382")
+  
   
