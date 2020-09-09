92bc92e6d584deaf6af1352a8e8f0c3cccac2eaa
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-09 11:16:32 -0400

    telephone validator with long regex to test the string

--- phonevalidator.js
+++ phonevalidator.js
@@ -7,4 +7,10 @@ function telephoneCheck(str) {
   
   telephoneCheck("1(555)555-5555")
   telephoneCheck("27576227382")
+
+  function telephoneCheck2(str) {
+    var regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+    return regex.test(str);
+  }
+  telephoneCheck2("555-555-5555");
   
