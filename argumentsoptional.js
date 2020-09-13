3b0b8475bbc8f656c1ff7fde0559dfd716622008
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-12 23:39:41 -0400

    second args optional algo

--- argumentsoptional.js
+++ argumentsoptional.js
@@ -11,3 +11,35 @@ function addTogether() {
   
   
   addTogether(5)(7)
+
+
+  function addTogether2() {
+    var checkNum = function(num) {
+      if (typeof num !== "number") {
+        return undefined;
+      } else return num;
+    };
+  
+    if (arguments.length > 1) {
+      var a = checkNum(arguments[0]);
+      var b = checkNum(arguments[1]);
+      if (a === undefined || b === undefined) {
+        return undefined;
+      } else {
+        return a + b;
+      }
+    } else {
+      var c = arguments[0];
+      if (checkNum(c)) {
+        return function(arg2) {
+          if (c === undefined || checkNum(arg2) === undefined) {
+            return undefined;
+          } else {
+            return c + arg2;
+          }
+        };
+      }
+    }
+  }
+  
+  addTogether2(2, 3);
