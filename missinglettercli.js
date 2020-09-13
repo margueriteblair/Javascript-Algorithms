386dca35e7c71dca373437fe938deb62c4983f8b
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-13 19:03:16 -0400

    used cli to return interactive missing letter finding algorithm

---
+++ missinglettercli.js
@@ -0,0 +1,17 @@
+const readLine = require('readline');
+const interface = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+interface.question("What is your phrase to compare?", (answer) => {
+    console.log(`Your missing letter is: ${missingLetter(answer)}`)
+})
+
+function missingLetter(str) {
+    for (let i = 0; i < str.length; i++) {
+        if (str.charCodeAt(i) - str.charCodeAt(i - 1) === 2) {
+          return String.fromCharCode(str.charCodeAt(i) - 1);
+        } 
+      }  
+}
