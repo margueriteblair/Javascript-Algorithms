acd2bae0186ea286c4e77ba0b89f321751fe84a2
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-14 13:10:44 -0400

    make a person with the setter methods being done differently

--- makeaperson.js
+++ makeaperson.js
@@ -30,4 +30,40 @@ let Person = function(firstAndLast) {
   
   var bob = new Person('Bob Ross');
   console.log(bob.setFullName('Haskell Curry'));
+
+
+  //second make a person attempts
+  var Person = function(firstAndLast) {
+    // Only change code below this line
+    // Complete the method below and implement the others similarly
+    let fullName = firstAndLast
+  
+    this.getFullName = function() {
+      return fullName;
+    };
+    this.getFirstName = function() {
+      return fullName.split(" ")[0]
+    }
+    this.getLastName = function() {
+      return fullName.split(" ")[1]
+    }
+    this.setFirstName = function(first) {
+      let nameArr = firstAndLast.split(" ")
+      nameArr.splice(0, 1, first)
+      fullName = nameArr.join(" ")
+      return fullName
+  };
+    this.setLastName = function(last) {
+      let nameArr = fullName.split(" ")
+      nameArr.splice(1, 1, last)
+      fullName = nameArr.join(" ")
+      return fullName;
+    }
+    this.setFullName = function(newFullName) {
+      fullName = newFullName;
+    }
+  }
+  var bob = new Person('Bob Ross');
+  bob.setFirstName("Haskell");
+  
   
