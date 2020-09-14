7d33bc56883f69fb081046c67f100048094c2861
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-14 12:06:04 -0400

    new ES6 solution to arguments optional algorithm

--- argumentsoptional.js
+++ argumentsoptional.js
@@ -43,3 +43,14 @@ function addTogether() {
   }
   
   addTogether2(2, 3);
+
+  function addTogether3() {
+    var args = Array.from(arguments);
+    return args.some(n => typeof n !== "number")
+      ? undefined
+      : args.length > 1
+      ? args.reduce((acc, n) => (acc += n), 0)
+      : n => (typeof n === "number" ? n + args[0] : undefined);
+  }
+  
+  addTogether3(2, 3);
