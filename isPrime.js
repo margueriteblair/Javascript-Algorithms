14edaa0a28d47e9aed169aecb90ea0487652d160
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-25 17:42:48 -0400

    shorter lines

--- isPrime.js
+++ isPrime.js
@@ -6,5 +6,16 @@ function isPrime(n) {
     }
     return true;
 }
-console.log(isPrime(127));
+console.log(isPrime(137));
 console.log(isPrime(237));
+
+function isPrime2(n) {
+    for (let i = 2; i < n; i++) {
+        if (n % i === 0) return false;
+    }
+    return true;
+}
+
+console.log(isPrime2(127));
+console.log(isPrime2(237));
+console.log(isPrime(7));
